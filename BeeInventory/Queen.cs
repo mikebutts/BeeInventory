725973ee5238e494dbd6c88d1fea64139a28219f
Michael Butts <mbutts3@wgu.edu>
2025-11-24 16:31:46 -0500

    basic layout and queen class added

---
+++ BeeInventory/Queen.cs
@@ -0,0 +1,9 @@
+﻿namespace BeeInventory
+{
+    public class Queen
+    {
+        public void AssignBee(string selection) { }
+        public void WorkTheNextShift() { }
+        public string StatusReport { get; set; } = " ";
+    }
+}
