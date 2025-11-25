9d576fae5fa38e53329cc6d7283324d830391934
Michael Butts <mbutts3@wgu.edu>
2025-11-25 01:43:21 -0500

    bee.cs and workerbee.cs updated

--- BeeInventory/Models/Bee.cs
+++ BeeInventory/Models/Bee.cs
@@ -1,14 +1,19 @@
-﻿namespace BeeInventory.Models
+﻿using BlazorBeehiveManagementSystem.Models;
+
+namespace BeeInventory.Models
 {
     public abstract class Bee
     {
-        public string Job { get; private set; } = "Idle";
+        public string Job { get; protected set; } = "Idle";
 
+        // How much honey this bee eats per shift
         public virtual int HoneyConsumptionRate => 1;
+
+        // Runs once per shift
+        // Returns a string describing what the bee accomplished.
         public abstract string Work(Hive hive);
+
+        // Used as a display name or identifier
         public override string ToString() => Job;
-
-
-
     }
 }
