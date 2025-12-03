d2956a441bd75f97b91d8c054d568bd7faf8def7
Michael Butts <mbutts3@wgu.edu>
2025-12-02 20:10:46 -0500

    updated Queen.cs with code.

--- BeeInventory/Models/Queen.cs
+++ BeeInventory/Models/Queen.cs
@@ -1,9 +1,59 @@
-﻿namespace BeeInventory.Models
+﻿using System.Text;
+
+namespace BeeInventory.Models
 {
     public class Queen
     {
-        public void AssignBee(string selection) { }
-        public bool WorkTheNextShift() => false;
-        public string StatusReport { get; set; } = " ";
+        private readonly Hive hive;
+
+        public Queen(Hive hive)
+        {
+            this.hive = hive;
+            StatusReport = "The queen is ready.";
+        }
+
+        public string StatusReport { get; private set; }
+
+        public void AssignBee(string selectedJob)
+        {
+            // Delegate the real work to the hive, then update the report
+            var assignmentResult = hive.AssignBee(selectedJob);
+
+            StatusReport = BuildStatusReport(
+                $"Assignment: {assignmentResult}"
+            );
+        }
+
+        
+        /// Runs the next shift. Returns false if the hive ran out of honey.
+       
+        public bool WorkTheNextShift()
+        {
+            string shiftReport = hive.WorkTheNextShift();
+            StatusReport = BuildStatusReport(shiftReport);
+
+            // Simple rule: hive is “dead” if honey < 0
+            return hive.Honey >= 0;
+        }
+
+        private string BuildStatusReport(string headerLine)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(headerLine);
+            sb.AppendLine();
+            sb.AppendLine($"Shift: {hive.ShiftNumber}");
+            sb.AppendLine($"Honey: {hive.Honey}");
+            sb.AppendLine($"Nectar: {hive.Nectar}");
+            sb.AppendLine($"Eggs: {hive.Eggs}");
+            sb.AppendLine();
+            sb.AppendLine($"Unassigned workers: {hive.UnassignedWorkers}");
+            sb.AppendLine($"Nectar collectors: {hive.NectarCollectors}");
+            sb.AppendLine($"Honey manufacturers: {hive.HoneyManufacturers}");
+            sb.AppendLine($"Egg care bees: {hive.EggCareBees}");
+            sb.AppendLine($"Total workers: {hive.TotalWorkers}");
+
+            return sb.ToString();
+        }
     }
 }
