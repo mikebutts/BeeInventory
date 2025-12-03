54273a5bdff4002ba8a9504aaed4e0245c89b261
Michael Butts <mbutts3@wgu.edu>
2025-12-02 20:44:41 -0500

    updated home, able to run, no results

--- BeeInventory/Models/HoneyManufacturer.cs
+++ BeeInventory/Models/HoneyManufacturer.cs
@@ -1,6 +1,34 @@
 ﻿namespace BeeInventory.Models
 {
-    public class HoneyManufacturer
+    public class HoneyManufacturer : WorkerBee
     {
+        // Book/GitHub numbers:
+        // Uses 2 nectar → produces 1 honey
+        private const int NectarConsumedPerShift = 2;
+        private const int HoneyProducedPerShift = 1;
+
+        public HoneyManufacturer() : base("Honey Manufacturer")
+        {
+        }
+
+        // All worker bees eat 1 honey per shift
+        public override int HoneyConsumptionRate => 1;
+
+        public override string Work(Hive hive)
+        {
+            // If we don't have enough nectar, do nothing
+            if (hive.Nectar < NectarConsumedPerShift)
+            {
+                return "Honey manufacturer found no nectar to convert.";
+            }
+
+            // Convert nectar → honey through Hive's helper
+            hive.ConvertNectarToHoney(
+                NectarConsumedPerShift,
+                HoneyProducedPerShift
+            );
+
+            return $"Honey manufacturer converted {NectarConsumedPerShift} nectar into {HoneyProducedPerShift} honey.";
+        }
     }
 }
--- BeeInventory/Models/NectarCollector.cs
+++ BeeInventory/Models/NectarCollector.cs
@@ -2,10 +2,20 @@
 {
     public class NectarCollector : WorkerBee
     {
+        // Book/GitHub value: collect 3 nectar per shift
         private const int NectarPerShift = 3;
 
-        public NectarCollector() : base("Nectar Collector") { }
+        public NectarCollector() : base("Nectar Collector")
         {
+        }
+
+        // All worker bees eat 1 honey per shift, same as other jobs
+        public override int HoneyConsumptionRate => 1;
+
+        public override string Work(Hive hive)
+        {
+            hive.CollectNectar(NectarPerShift);
+            return $"Nectar collector gathered {NectarPerShift} nectar.";
         }
     }
 }
